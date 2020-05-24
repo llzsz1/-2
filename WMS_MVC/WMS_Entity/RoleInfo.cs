ded48d4c33f2ef97c28db75a33a83be3dbbd4802
张凯 <56509647@qq.com>
2020-05-24 18:37:04 +0800

    登录

---
+++ WMS_MVC/WMS_Entity/RoleInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace WMS_Entity
+{
+    public partial class RoleInfo
+    {
+        public int RoleId { get; set; }
+        public string RoleName { get; set; }
+        public string RoleType { get; set; }
+        public bool IsDel { get; set; }
+        public string Remark { get; set; }
+        public int CreateBy { get; set; }
+        public DateTime CreateDate { get; set; }
+        public int ModifiedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
--- WMS_MVC/WMS_MVC/Startup.cs
+++ WMS_MVC/WMS_MVC/Startup.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WMS_MVC.Code;
 
 namespace WMS_MVC
 {
@@ -22,7 +24,10 @@ namespace WMS_MVC
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddSingleton(typeof(VerificationCodeAESHelp));
             services.AddControllersWithViews();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,16 +42,18 @@ namespace WMS_MVC
                 app.UseExceptionHandler("/Home/Error");
             }
             app.UseStaticFiles();
+            app.UseCookiePolicy();
 
             app.UseRouting();
 
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
+            app.UseEndpoints(route =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                route.MapControllerRoute(
+                   name: "default",
+                   pattern: "{controller=Home}/{action=Index}/{id?}");
+                
+                route.MapRazorPages();
+
             });
         }
     }
