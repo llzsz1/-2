2aae5b718c408e39eb596fad6423ea53b5f2c50b
张凯 <56509647@qq.com>
2020-06-05 14:11:32 +0800

    11

---
+++ WMS_MVC/WMS_MVC/Controllers/ReservoirareaController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WMS_MVC.Controllers
+{
+    public class ReservoirareaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
